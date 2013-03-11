84e84dba05525ec190eb7bed360aaea79371278f
Kevin Whitman <keverw92@gmail.com>
2013-03-10 20:05:43 -0400

    First commit!

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Text.RegularExpressions;
+
+namespace pathUsername
+{
+    class Program
+    {
+        static string pathUsername(string path)
+        {
+            string userName = Environment.UserName;
+            string value = Regex.Replace(path, "%username%", userName, RegexOptions.IgnoreCase);
+
+            return value;
+        }
+
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/%username%/Desktop/simBackups";
+            Console.WriteLine(pathUsername(path));
+            Console.ReadLine();
+        }
+    }
+}
